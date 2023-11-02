55063c746f81602746b8f4a97b304f968ecacbbf
BeauTheBeau <beauthebeau@proton.me>
2023-11-02 01:46:13 +0000

    feat: Created a new userModel.js

---
+++ src/models/userModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    user_id: { type: String, required: true },
+    points: { type: Number, default: 0 },
+
+    // Format: [ "question", "amountOfTimes", "category" ]
+    correct_answers: { type: Array, default: [] },
+    incorrect_answers: { type: Array, default: [] },
+
+    last_question: { type: String, default: '' },
+    last_question_correct: { type: Boolean, default: false },
+});
+
+module.exports = mongoose.model('User', userSchema);
