dbb593e31eec13b94859f1790a2f03f52c2a1c33
BeauTheBeau <beauthebeau@proton.me>
2023-11-18 21:44:38 +0000

    fix: Remove typo

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -62,7 +62,7 @@ class Logger {
 
 
 
-    getFormattedFileName(name) {v
+    getFormattedFileName(name) {
         const now = new Date();
         const date = now.toISOString().slice(0, 10);
         const time = now.toTimeString().slice(0, 8).replace(/:/g, '-');
